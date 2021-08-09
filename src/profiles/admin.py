07af1029e73c941665f590c7841da3405327ab23
FaisalWant <heliconrngr@gmail.com>
2021-08-09 18:54:39 +0530

    0.2 -Added the Profile Model and registered the Previous Models

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
+from .models import Profile
 
+admin.site.register(Profile)
 # Register your models here.
-from .models import Product
-
-admin.site.register(Product)
